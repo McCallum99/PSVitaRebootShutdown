5e9c9ddff9bd0eec92b8d286b9b9a8f3180f00ae
Callum <42181838+McCallum99@users.noreply.github.com>
2018-08-07 21:30:04 +0100

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -1,18 +1,11 @@
 #include <psp2/ctrl.h>
-#include <psp2/kernel/processmgr.h>
-
-int main () {
+int main(){
     SceCtrlData pad;
-
     while (1) {
     sceCtrlPeekBufferPositive(0, &pad, 1);
-
-    if (pad.buttons & SCE_CTRL_RTRIGGER) {
+    if (pad.buttons & SCE_CTRL_RTRIGGER)
             scePowerRequestColdReset();
-    }
-
-    if (pad.buttons & SCE_CTRL_LTRIGGER) {
+    if (pad.buttons & SCE_CTRL_LTRIGGER)
             scePowerRequestStandby();
     }
-}
 }
