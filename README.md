3f368f46c6a2410715a58526123709ecdb024ebd
Callum <42181838+McCallum99@users.noreply.github.com>
2019-01-08 23:01:48 +0000

    Updated the README.md
    Folder layout has once again changed.

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # PSVitaRebootShutdown
-This is an app that reboots or powers off your PS Vita depending on what trigger you hold. Hold the right trigger to reboot, hold the left trigger to shutdown.
+This is an app that reboots or powers off your PS Vita depending on what trigger you hold. Hold the right trigger to reboot, hold the left trigger to shutdown. You can now also press X and □ to update the database (keep icons) or left and up on the d-pad to delete the database and restore it (lose icons).
 
 To compile, run the following from inside the folder with "CMakeLists.txt": 
 cmake . && make
 
-This is v1.04. 
+This is v1.05. 
 Thanks for using my app! By Mc_Callum_99
+
+Thank you SilicaAndPina (@VitaDevs) for the help troubleshooting - I'm new to this okay?
 
--- src/main.c
+++ src/main.c
@@ -1,19 +1,24 @@
-#include <psp2/kernel/threadmgr.h>
-#include <psp2/kernel/processmgr.h>
 #include <stdio.h>
-#include "debugScreen.h"
 #include <psp2/ctrl.h>
-#define printf psvDebugScreenPrintf
+#include <psp2/io/fcntl.h>
+#include <psp2/power.h>
+#include "debugScreen.h"
 
-int main(int argc, char *argv[]) {
+#define printf  psvDebugScreenPrintf
+
+int main(int argc, const char *argv[]) {
     psvDebugScreenInit();
-	printf("Press the left trigger to shut down or the right trigger to reboot. ");
+    printf("Press the left trigger to shut down or the right trigger to reboot.\n\n\n\nPress CROSS and SQUARE to update the database (keep icon layout).\n\n\n\nPress LEFT and UP on the d-pad to delete the database (lose icon layout).");
     while (1) {
-		SceCtrlData pad;
+	SceCtrlData pad;
     	sceCtrlPeekBufferPositive(0, &pad, 1);
     	if (pad.buttons & SCE_CTRL_RTRIGGER)
             scePowerRequestColdReset();
     	if (pad.buttons & SCE_CTRL_LTRIGGER)
             scePowerRequestStandby();
-    }
+        if (pad.buttons & SCE_CTRL_CROSS + SCE_CTRL_SQUARE)
+	    sceIoRemove("ux0:/id.dat") & scePowerRequestColdReset();
+	if (pad.buttons & SCE_CTRL_UP + SCE_CTRL_LEFT)
+	    sceIoRemove("ur0:/shell/db/app.db") & scePowerRequestColdReset();
+    }	
 }
